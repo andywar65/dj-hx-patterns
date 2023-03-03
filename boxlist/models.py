144bfcbe0fd5ff324c68370b78d47c176ab177e0
andywar65 <andy.war1965@gmail.com>
2023-03-03 19:54:50 +0100

    adding item model

--- boxlist/models.py
+++ boxlist/models.py
@@ -1,3 +1,20 @@
-from django.db import models  # noqa
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(
+        _("Name"),
+        max_length=50,
+    )
+    position = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        verbose_name = _("Item")
+        verbose_name_plural = _("Items")
+        ordering = [
+            "position",
+        ]
+
+    def __str__(self):
+        return self.title

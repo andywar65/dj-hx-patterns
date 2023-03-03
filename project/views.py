a6588f4f3f87ae0c9ffbfa6b7c207e1020deed91
andywar65 <andy.war1965@gmail.com>
2023-03-03 18:39:37 +0100

    adding reusable mixins

--- project/views.py
+++ project/views.py
@@ -1,4 +1,23 @@
+from django.http import Http404
 from django.views.generic import TemplateView
+
+
+class HxPageTemplateMixin:
+    """Switches template depending on request.htmx"""
+
+    def get_template_names(self):
+        if not self.request.htmx:
+            return [self.template_name.replace("htmx/", "")]
+        return [self.template_name]
+
+
+class HxOnlyTemplateMixin:
+    """Restricts view to HTMX requests"""
+
+    def get_template_names(self):
+        if not self.request.htmx:
+            raise Http404("Request without HTMX headers")
+        return [self.template_name]
 
 
 class BaseTemplateView(TemplateView):

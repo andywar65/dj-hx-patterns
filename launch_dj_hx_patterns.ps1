172768f23f2ef085f629aacc1d3b3ede2a07aeb9
andywar65 <andy.war1965@gmail.com>
2023-03-05 12:12:44 +0100

    added powershell launch routine

---
+++ launch_dj_hx_patterns.ps1
@@ -0,0 +1,17 @@
+cd D:/venvs/
+mkdir my_directory
+cd my_directory
+py -3.10 -m virtualenv env
+env/Scripts/activate
+mkdir static
+mkdir media
+python -m pip install pip-tools
+git clone https://github.com/andywar65/dj-hx-patterns
+cd dj-hx-patterns
+python -m pip install -r requirements.txt
+pre-commit install
+python generate_env_file.py
+python manage.py migrate
+python manage.py create_super_user
+python manage.py seed_items
+.\run

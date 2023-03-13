de4e4c67b59c01467dc30168cc26da37702572b1
andywar65 <andy.war1965@gmail.com>
2023-03-13 20:49:37 +0100

    adding category model

--- hierarchy/admin.py
+++ hierarchy/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "parent", "position")
+    list_filter = ("parent",)
+    list_editable = ("position",)
--- hierarchy/models.py
+++ hierarchy/models.py
@@ -1,3 +1,16 @@
-from django.db import models  # noqa
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from tree_queries.models import TreeNode
 
-# Create your models here.
+
+class Category(TreeNode):
+    title = models.CharField(
+        _("Name"),
+        max_length=50,
+    )
+    position = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+        ordering = ["position"]

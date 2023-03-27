9f4a4f23e37ff3178eaf2c79ecca8b92d7ba68a6
andywar65 <andy.war1965@gmail.com>
2023-03-27 19:22:05 +0200

    boxlist model tests covered

--- boxlist/tests.py
+++ boxlist/tests.py
@@ -1,14 +1,12 @@
-from django.test import TestCase, override_settings
+from django.test import TestCase
 
 from .models import Item
 
 
-@override_settings(USE_I18N=False)
 class ItemModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        print("\nTest boxlist models")
-        # Set up objects used by test methods
+        print("\nTest unmodified boxlist models")
         Item.objects.create(position=1, title="First")
         Item.objects.create(position=2, title="Central")
         Item.objects.create(position=3, title="Last")
@@ -28,18 +26,30 @@ class ItemModelTest(TestCase):
         self.assertEquals(it3.get_previous_item(), it2)
         print("\n-Test next / previous Item")
 
-    def test_move_up_down_item(self):
-        it1 = Item.objects.get(title="First")
+
+class ItemModifiedModelTest(TestCase):
+    def setUp(self):
+        print("\nTest modified boxlist models")
+        Item.objects.create(position=1, title="First")
+        Item.objects.create(position=2, title="Central")
+
+    def test_move_up_item(self):
         it2 = Item.objects.get(title="Central")
         it2.move_up()
+        it1 = Item.objects.get(title="First")
         self.assertEquals(it1.position, 2)
-        it2.move_down()
-        self.assertEquals(it1.position, 1)
-        print("\n-Test move up / down Item")
+        print("\n-Test move up Item")
+
+    def test_move_down_item(self):
+        it1 = Item.objects.get(title="First")
+        it1.move_down()
+        it2 = Item.objects.get(title="Central")
+        self.assertEquals(it2.position, 1)
+        print("\n-Test move down Item")
 
     def test_move_following_items(self):
         it1 = Item.objects.get(title="First")
-        it2 = Item.objects.get(title="Central")
         it1.move_following_items()
+        it2 = Item.objects.get(title="Central")
         self.assertEquals(it2.position, 1)
         print("\n-Test move following Items")

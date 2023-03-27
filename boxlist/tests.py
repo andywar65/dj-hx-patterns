0c199562a0a1444b623bda9e6be5215255a58703
andywar65 <andy.war1965@gmail.com>
2023-03-27 12:54:50 +0200

    test str method

--- boxlist/tests.py
+++ boxlist/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase  # noqa
+from django.test import TestCase, override_settings
 
-# Create your tests here.
+from .models import Item
+
+
+@override_settings(USE_I18N=False)
+class ItemModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("\nTest boxlist models")
+        # Set up objects used by test methods
+        Item.objects.create(position=1, title="First")
+        Item.objects.create(position=2, title="Central")
+        Item.objects.create(position=3, title="Last")
+
+    def test_homepage_str(self):
+        it1 = Item.objects.get(title="First")
+        self.assertEquals(it1.__str__(), "First")
+        print("\n-Test Item title")

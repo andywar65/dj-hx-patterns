673ba06f8884d424eea592e6ae53062b6b33ba33
andywar65 <andy.war1965@gmail.com>
2023-04-20 13:55:15 +0200

    added more attributes

--- project/templatetags/htmx_tags.py
+++ project/templatetags/htmx_tags.py
@@ -1,3 +1,5 @@
+import json
+
 from django import template
 from django.utils.safestring import mark_safe
 
@@ -20,7 +22,16 @@ def htmx_csrf(token):
 
 
 METHODS = ["post", "POST", "put", "PUT", "patch", "PATCH", "delete", "DELETE"]
-SWAP = ["innerHTML", "outerHTML", "none"]
+SWAP = [
+    "innerHTML",
+    "outerHTML",
+    "afterbegin",
+    "beforebegin",
+    "afterend",
+    "beforeend",
+    "delete",
+    "none",
+]
 
 
 @register.simple_tag
@@ -39,10 +50,24 @@ def htmx_request(link_dict):
         if not link_dict["target"].startswith("#"):
             link_dict["target"] = "#" + link_dict["target"]
         output += 'hx-target="%(target)s" ' % {"target": link_dict["target"]}
+    if "include" in link_dict:
+        if not link_dict["include"].startswith("#"):
+            link_dict["include"] = "#" + link_dict["include"]
+        output += 'hx-include="%(include)s" ' % {"include": link_dict["include"]}
     if "swap" in link_dict and link_dict["swap"] in SWAP:
         output += 'hx-swap="%(swap)s" ' % {"swap": link_dict["swap"]}
     if "push" in link_dict:
         output += 'hx-push-url="%(push)s" ' % {"push": link_dict["push"]}
     if "trigger" in link_dict:
         output += 'hx-trigger="%(trigger)s" ' % {"trigger": link_dict["trigger"]}
+    if "sync" in link_dict:
+        output += 'hx-sync="%(sync)s" ' % {"sync": link_dict["sync"]}
+    if "confirm" in link_dict:
+        output += 'hx-confirm="%(confirm)s" ' % {"confirm": link_dict["confirm"]}
+    if "headers" in link_dict and isinstance(dict, link_dict["headers"]):
+        output += "hx-headers='%(headers)s' " % {
+            "headers": json.dumps(link_dict["headers"])
+        }
+    if "vals" in link_dict and isinstance(dict, link_dict["vals"]):
+        output += "hx-vals='%(vals)s' " % {"vals": json.dumps(link_dict["vals"])}
     return mark_safe(output)

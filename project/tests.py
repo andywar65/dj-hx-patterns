2b8782b7359132275472ca819b3da522bad502c6
andywar65 <andy.war1965@gmail.com>
2023-05-01 14:05:38 +0200

    adding tests for template tags

--- project/tests.py
+++ project/tests.py
@@ -1,3 +1,4 @@
+from django.template import Context, Template
 from django.test import TestCase
 from django.urls import reverse
 
@@ -9,3 +10,10 @@ class ProjectViewTest(TestCase):
         print("\n-Test base status 200")
         self.assertTemplateUsed(response, "base.html")
         print("\n-Test base template")
+
+
+class ProjectTemplateTagsTest(TestCase):
+    def test_htmx_cdn_tag(self):
+        out = Template("{% load htmx_tags %}" "{% htmx_cdn %}").render(Context())
+        self.assertIn("https://unpkg.com/htmx.org@", out)
+        print("\n-Test htmx cdn tag")

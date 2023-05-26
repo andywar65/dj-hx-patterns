2cca406ee3b5c4a40c51c2c1b5ee13af5ff01a8c
andywar65 <andy.war1965@gmail.com>
2023-05-26 20:07:42 +0200

    debug setting in env file

--- project/settings.py
+++ project/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
